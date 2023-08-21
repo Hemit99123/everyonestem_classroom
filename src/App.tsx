f3f26b3f18d0a3219843b780ff5b94af15abdcf5
Hemit Patel <itshp404@gmail.com>
2023-08-21 10:45:55 -0400

    first verison

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Box, Alert, AlertIcon, AlertTitle, AlertDescription, Text} from '@chakra-ui/react'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Text
+      fontSize='4xl'
+      fontWeight='bold'
+      padding={5}
+    >
+      Everyone Classroom
+    </Text>
+    <Box
+    display="flex"
+    justifyContent="center"
+    alignItems="center"
+    minHeight="100vh"
+  >
+  
+<Alert
+  status='error'
+  variant='subtle'
+  flexDirection='column'
+  alignItems='center'
+  justifyContent='center'
+  textAlign='center'
+  height='200px'
+>
+  <AlertIcon boxSize='40px' mr={0} />
+  <AlertTitle mt={4} mb={1} fontSize='lg'>
+    Currently under devlopment
+  </AlertTitle>
+  <AlertDescription maxWidth='sm'>
+    Thanks for your interest. Our dev team is working hard to develop this! For now, use our temporary solution at <a href="https://www.everyonestem.org/courses">here</a>
+  </AlertDescription>
+</Alert>
+  </Box>
+  </>
+  )
 }
 
-export default App;
+export default App
